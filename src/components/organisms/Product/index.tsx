2f456c5ef4e08ff3b85a3d784ce52928026ef378
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-05-02 13:42:40 -0300

    breadcrumb

---
+++ src/components/organisms/Product/index.tsx
@@ -0,0 +1,12 @@
+import Breadcrumb from "../../atoms/Breadcrumb";
+import "./styles.scss";
+
+const Product = () => {
+    return (
+        <section className="product">
+            <Breadcrumb />
+        </section>
+    );
+};
+
+export default Product;
--- src/template/index.tsx
+++ src/template/index.tsx
@@ -1,10 +1,12 @@
 import HeaderHero from "../components/organisms/HeaderHero";
+import Product from "../components/organisms/Product";
 import "./styles.scss";
 
 export default function Front() {
     return (
         <main>
             <HeaderHero />
+            <Product />
         </main>
     );
 }
