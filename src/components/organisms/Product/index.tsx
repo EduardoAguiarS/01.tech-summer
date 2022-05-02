3f47d457ff3abcd6eff28223ac680e8d448ea84f
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-05-02 15:00:43 -0300

    restart product

--- src/components/organisms/Product/index.tsx
+++ src/components/organisms/Product/index.tsx
@@ -6,13 +6,7 @@ const Product = () => {
     return (
         <section className="product">
             <Breadcrumb />
-            <ProductInfo
-                title={"Rasteira Tira Dedo"}
-                code={"RT 0568 | 03.07.0653"}
-                price={69.9}
-                priceOffer={55.2}
-                vezes={6}
-            />
+            <ProductInfo />
         </section>
     );
 };
