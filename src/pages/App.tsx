a2e479950f6196218d5f1057c5187486a7ac54a6
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-05-02 09:13:01 -0300

    template

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,11 @@
+import Front from "../template";
+
 function App() {
-    return <div className="App">Testando</div>;
+    return (
+        <div className="App">
+            <Front />
+        </div>
+    );
 }
 
 export default App;
