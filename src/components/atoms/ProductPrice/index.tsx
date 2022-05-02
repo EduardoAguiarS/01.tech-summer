92efca86b58e7bea28159b9a1d5205597bcc6fd8
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-05-02 16:34:45 -0300

    price components

--- src/components/atoms/ProductPrice/index.tsx
+++ src/components/atoms/ProductPrice/index.tsx
@@ -8,12 +8,16 @@ interface PriceProps {
 
 const ProductPrice = ({ price, offer, portion }: PriceProps) => {
     return (
-        <div>
-            <div>
-                <span>R$ {price.toFixed(2).replace(".", ",")}</span>{" "}
-                <span>R$ {offer.toFixed(2).replace(".", ",")}</span>
+        <div className="price__container">
+            <div className="product__price">
+                <span className="price">
+                    R$ {price.toFixed(2).replace(".", ",")}
+                </span>{" "}
+                <span className="offer">
+                    R$ {offer.toFixed(2).replace(".", ",")}
+                </span>
             </div>
-            <div>
+            <div className="portion">
                 <span>
                     Ou {portion}x de R${" "}
                     {(offer / portion).toFixed(2).replace(".", ",")}
