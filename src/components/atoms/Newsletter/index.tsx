168a1d51715612308c85c68637f979a7a727b27a
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-05-04 15:38:47 -0300

    footer mobile news

--- src/components/atoms/Newsletter/index.tsx
+++ src/components/atoms/Newsletter/index.tsx
@@ -1,23 +1,26 @@
 import Button from "../Buttons";
+import "./styles.scss";
 
 const Newsletter = () => {
     return (
-        <aside className="newslatter">
+        <aside className="newsletter">
             <h3>Assine nossa news</h3>
-            <label htmlFor="nome">Nome</label>
-            <input type="text" id="nome" />
-            <div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" />
+            <div className="news__form">
+                <label htmlFor="nome">Nome</label>
+                <input type="text" id="nome" />
+                <div className="email">
+                    <div className="email__input">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" />
+                    </div>
+                    <Button
+                        text={"Enviar"}
+                        color={"white"}
+                        bgColor={"#DE8F75"}
+                        size={"btn__small"}
+                        full={false}
+                    />
                 </div>
-                <Button
-                    text={"Enviar"}
-                    color={"white"}
-                    bgColor={"#DE8F75"}
-                    size={"btn__small"}
-                    full={false}
-                />
             </div>
         </aside>
     );
