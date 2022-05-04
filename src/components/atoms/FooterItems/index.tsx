d823803ae2f32ce87718de48a9d832a81de99d6c
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-05-04 14:29:36 -0300

    footer sections

--- src/components/atoms/FooterItems/index.tsx
+++ src/components/atoms/FooterItems/index.tsx
@@ -1,7 +1,11 @@
 import "./styles.scss";
 
 const FooterItems = ({ text }: any) => {
-    return <li className="footer__item">{text}</li>;
+    return (
+        <li className="footer__item">
+            <a href="#">{text}</a>
+        </li>
+    );
 };
 
 export default FooterItems;
--- src/components/organisms/footer/index.tsx
+++ src/components/organisms/footer/index.tsx
@@ -1,4 +1,5 @@
 import FooterFooter from "../../atoms/FooterFooter";
+import FooterItems from "../../atoms/FooterItems";
 import FooterSocial from "../../atoms/FooterSocial";
 import FooterTitle from "../../atoms/FooterTitles";
 import "./styles.scss";
@@ -6,8 +7,35 @@ import "./styles.scss";
 const Footer = () => {
     return (
         <footer className="footer__container">
-            <FooterSocial />
-            <FooterTitle text={"Testando"} />
+            <div className="footer__comp">
+                <FooterSocial />
+                <div className="footer_sections">
+                    <FooterTitle text={"Institucional"} />
+                    <ul className="footer__list">
+                        <FooterItems text={"A Marca"} />
+                        <FooterItems text={"Lojas"} />
+                        <FooterItems text={"Contato"} />
+                    </ul>
+                </div>
+
+                <div className="footer_sections">
+                    <FooterTitle text={"Informações"} />
+                    <ul className="footer__list">
+                        <FooterItems text={"Formas de Pagamento"} />
+                        <FooterItems text={"Trocas e Devoluções "} />
+                        <FooterItems text={"Cuidados Com o Produto"} />
+                    </ul>
+                </div>
+
+                <div className="footer_sections">
+                    <FooterTitle text={"Conheça"} />
+                    <ul className="footer__list">
+                        <FooterItems text={"Franquias e Multimarcas"} />
+                        <FooterItems text={"Trabalhe com a Gente"} />
+                        <FooterItems text={"Procon-RJ"} />
+                    </ul>
+                </div>
+            </div>
             <FooterFooter />
         </footer>
     );
